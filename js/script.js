aa26fffd6bb0d54fb8fb5aab152a96154537cf94
Michele Cofano <brutalmachinelol@gmail.com>
2020-12-16 15:42:48 +0100

    Aggiunto Array lista cognomi + richiesta prompt del cognome

--- js/script.js
+++ js/script.js
@@ -4,3 +4,12 @@
     // Chiedere all'utente il proprio cognome
       // Stampare la lista ordinata in ordine alfabetico
         //
+
+// lista dei cognomi (array)
+var cognomi = ['cofano', 'fogliata', 'guida', 'gennari', 'colloca', 'del piero', 'baggio', 'greco', 'ronaldo', 'gattuso'];
+
+// Chiedere all'utente il suo cognome
+var cognomeUser = prompt('Ciao, inserisci il tuo cognome');
+while (cognomeUser.length <= 1) {
+  cognomeUser = prompt('Ciao, inserisci il tuo cognome');
+}
